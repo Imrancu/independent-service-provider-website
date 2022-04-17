bd3c3e2d27af73c4096ffaf43cd550f6a589ced7
imrancu <mdimran3772@gmail.com>
2022-04-18 04:34:01 +0600

    Home page responsive done

--- src/Components/Home/Sample/Sample.jsx
+++ src/Components/Home/Sample/Sample.jsx
@@ -4,17 +4,17 @@ import './Sample.css';
 
 const Sample = () => {
     return (
-        <div className='container mb-5'>
-            <h1 className='text-success border-bottom mb-5'>Dentist Work</h1>
-            <div className="row content">
-                <div className="text">
+        <div className="container">
+            <h1 className='border-bottom text-success my-5'>Feature</h1>
+            <div className="row d-flex align-items-center justify-content-center">
+                <div className="col-md-6 col-sm-12">
                     <div>
                         <h1 className='text-success text-start mb-4'>Implants</h1>
                         <p className='text-start fs-5 text-dark'>Missing teeth can make it hard for you to eat your favorite foods or smile without feeling self-conscious. Implants are the best way to fix missing teeth forever.</p>
                     </div>
                 </div>
-                <div className="image">
-                    <img src={img} alt="" />
+                <div className="col-md-6 col-sm-12 my-5">
+                    <img src={img} className="img-fluid shadow-sm p-3 mb-5 bg-body rounded" alt="" />
                 </div>
             </div>
         </div>
--- src/Components/Home/Service/Service.jsx
+++ src/Components/Home/Service/Service.jsx
@@ -5,11 +5,11 @@ const Service = ({ service }) => {
     const { serviceName, description, price } = service;
     return (
         <div className='col-sm-12 col-md-6 col-lg-4 g-5 bg-light'>
-            <div className="card hover-style border-0 shadow-sm p-3 mb-5 bg-body rounded" style={{ width: "18rem;" }}>
+            <div className="card hover-style border-0 shadow-sm p-3 mb-5 bg-body rounded">
                 <div className="card-body">
                     <h5 className="card-title">{serviceName}</h5>
-                    <p className="card-text text-dark">{description}</p>
-                    <p className=''><small>{price}</small></p>
+                    <p title={description} className="card-text text-dark">{description.slice(0, 200)}...</p>
+                    <p className='text-dark'><small>{price}</small></p>
                     <a href="#" className="btn btn-color">Book Now</a>
                 </div>
             </div>
